e03ff7972e6e2d6e454176c3432aa012844907f8
paul.kelly <pkelly@equalexperts.com>
2014-05-28 16:59:51 +0100

    With hardcoded line stlying..shame shame.

--- Gruntfile.js
+++ Gruntfile.js
@@ -85,7 +85,7 @@ module.exports = function(grunt) {
 
         connect: {
             options: {
-                port: 9000,
+                port: 9500,
                 // change this to '0.0.0.0' to access the server from outside
                 hostname: 'localhost'
             },
